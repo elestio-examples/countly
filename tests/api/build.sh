156f27ebe9d2bf27816f5fa0ea3cc48adcf17492
AmsellemJoseph <amsellem.joseph@gmail.com>
2024-04-21 13:47:31 +0300

    fix

--- tests/api/build.sh
+++ tests/api/build.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
+rm -f ./Dockerfile
 cp ./Dockerfile-api ./Dockerfile
 docker buildx build . --output type=docker,name=elestio4test/countly-api:latest | docker load
--- tests/frontend/build.sh
+++ tests/frontend/build.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
+rm -f ./Dockerfile
 cp ./Dockerfile-frontend ./Dockerfile
 docker buildx build . --output type=docker,name=elestio4test/countly-frontend:latest | docker load
